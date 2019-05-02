53a4335935577d1240dbbc82b6a896a030cbfc36
Harsh Patel <hypharsh@gmail.com>
2019-05-01 21:40:02 -0500

    Alot of UI added

--- app/src/main/java/com/example/myapplication/activityTabActivity.java
+++ app/src/main/java/com/example/myapplication/activityTabActivity.java
@@ -4,43 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.TableRow;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+public class activityTabActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_tab);
     }
 
 
-    //adding the items
     public void addClick(View view){
-        startActivity(new Intent(MainActivity.this , Bill.class));
+        startActivity(new Intent(activityTabActivity.this , Bill.class));
     }
-
 
     //go to friend tab
     public void  friendClick(View view){ // MainActivity and Friends is the same.
-        startActivity(new Intent(MainActivity.this , MainActivity.class));
+        startActivity(new Intent(activityTabActivity.this , MainActivity.class));
     }
 
     //go to groups tab
 
     public void groupsClick(View view){
-        startActivity(new Intent(MainActivity.this , Groups.class));
+        startActivity(new Intent(activityTabActivity.this , Groups.class));
     }
 
     //go to activity tab
 
     public void activityClick(View view){
-        startActivity(new Intent(MainActivity.this , activityTabActivity.class));
+        startActivity(new Intent(activityTabActivity.this , activityTabActivity.class));
     }
-
 }
